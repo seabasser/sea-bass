98452b0bcfb4005e315bcb1f6864e52d9b445890
Chris Mitchell <cmitchel@redhat.com>
2021-08-16 18:14:38 -0400

    Docs are good, I guess?
    Signed-off-by: Chris Mitchell <cmitchel@redhat.com>

--- server.py
+++ server.py
@@ -27,13 +27,117 @@ def booze():
 
 @app.route("/drinks", methods=["GET"])
 def drinks():
-    booze = request.args.get('base')
-    result = barreleye.get_drinks_by_booze(booze)
+    """Get drinks based on provided spirit.
+
+    @api {get} /drinks/   Get a drink based on spirit
+    @apiName getDrinks
+    @apiVersion 1.0.0
+    @apiDescription Get drinks from base spirit
+
+    @apiParam (Query) {String} spirit Base spirit of desired drinks
+
+    @apiSuccess {int} count Number of drinks returned
+    @apiSuccess {Array} drinks Array of drink dicts based on spirit
+    @apiSuccessExample {json} Success-Response:
+        HTTP/1.1 200 SUCCESS
+        {
+            "count": 3,
+            "drinks": [
+                {
+                    "strDrink": "Damned if you do",
+                    "strDrinkThumb": "https://www.thecocktaildb.com/images/media/drink/ql7bmx1503565106.jpg",
+                    "idDrink": "13194"
+                },
+                {
+                    "strDrink": "Hot Toddy",
+                    "strDrinkThumb": "https://www.thecocktaildb.com/images/media/drink/ggx0lv1613942306.jpg",
+                    "idDrink": "178345"
+                },
+                {
+                    "strDrink": "Owen's Grandmother's Revenge",
+                    "strDrinkThumb": "https://www.thecocktaildb.com/images/media/drink/0wt4uo1503565321.jpg",
+                    "idDrink": "13200"
+                }
+            ]
+        }
+    """
+    spirit = request.args.get('spirit')
+    result = barreleye.get_drinks_by_booze(spirit)
     return Response(response=result, mimetype='application/json')
 
 
 @app.route("/spec", methods=["GET"])
 def spec():
+    """Get drink spec from cocktailDB drink id.
+
+    @api {get} /spec/   Get a spec based on drink id
+    @apiName getSpec
+    @apiVersion 1.0.0
+    @apiDescription Get drink spec by cocktailDB drink id
+
+    @apiParam (Query) {String} id cocktailDB drink id to find spec
+
+    @apiSuccess {Array} drinks Array of drink details based on id
+    @apiSuccessExample {json} Success-Response:
+        HTTP/1.1 200 SUCCESS
+        {
+            "drinks": [
+                {
+                    "idDrink": "13194",
+                    "strDrink": "Damned if you do",
+                    "strDrinkAlternate": null,
+                    "strTags": null,
+                    "strVideo": null,
+                    "strCategory": "Shot",
+                    "strIBA": null,
+                    "strAlcoholic": "Alcoholic",
+                    "strGlass": "Shot glass",
+                    "strInstructions": "Pour into shot glass. Put in mouth. Repeat as deemed necessary.",
+                    "strInstructionsES": null,
+                    "strInstructionsDE": "In das Schnapsglas gießen. In den Mund nehmen. Bei Bedarf wiederholen.",
+                    "strInstructionsFR": null,
+                    "strInstructionsIT": "Versare nel bicchiere da shot.\r\nVersa in bocca.",
+                    "strInstructionsZH-HANS": null,
+                    "strInstructionsZH-HANT": null,
+                    "strDrinkThumb": "https://www.thecocktaildb.com/images/media/drink/ql7bmx1503565106.jpg",
+                    "strIngredient1": "Whiskey",
+                    "strIngredient2": "Hot Damn",
+                    "strIngredient3": null,
+                    "strIngredient4": null,
+                    "strIngredient5": null,
+                    "strIngredient6": null,
+                    "strIngredient7": null,
+                    "strIngredient8": null,
+                    "strIngredient9": null,
+                    "strIngredient10": null,
+                    "strIngredient11": null,
+                    "strIngredient12": null,
+                    "strIngredient13": null,
+                    "strIngredient14": null,
+                    "strIngredient15": null,
+                    "strMeasure1": "0.75 oz ",
+                    "strMeasure2": "0.25 oz ",
+                    "strMeasure3": null,
+                    "strMeasure4": null,
+                    "strMeasure5": null,
+                    "strMeasure6": null,
+                    "strMeasure7": null,
+                    "strMeasure8": null,
+                    "strMeasure9": null,
+                    "strMeasure10": null,
+                    "strMeasure11": null,
+                    "strMeasure12": null,
+                    "strMeasure13": null,
+                    "strMeasure14": null,
+                    "strMeasure15": null,
+                    "strImageSource": null,
+                    "strImageAttribution": null,
+                    "strCreativeCommonsConfirmed": "No",
+                    "dateModified": "2017-08-24 09:58:26"
+                }
+            ]
+        }
+    """
     drink_id = request.args.get('id')
     result = barreleye.get_recipe_by_id(drink_id)
     return Response(response=result, mimetype='application/json')

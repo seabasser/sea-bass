c4d22f40718922dfeb923e50e5a20f48082fcff6
Stephen Adams <tsadams@gmail.com>
2021-08-17 11:17:44 -0400

    [fix] update after rebase for new routes
    Signed-off-by: Stephen Adams <tsadams@gmail.com>

--- app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from app.handlers.routes import configure_routes
-
-app = Flask(__name__)
-
-configure_routes(app)
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)

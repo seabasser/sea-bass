0d0c2115807e9f71b6582fcec406d0fa1ec0e87d
Alec Cohan <alcohan@redhat.com>
2021-08-17 12:36:20 -0400

    fix inting for lambda functions

--- utils/sort.py
+++ utils/sort.py
@@ -10,10 +10,10 @@ def filter_and_sort(result, drink_type):
         for drink in result:
             if drink["Type"] == drink_type:
                 response.append(drink)
-        response.sort(key=lambda x:x["Retail Bottle Price"])  # Sort by Retail Price
+        response.sort(key=lambda x: x["Retail Bottle Price"])  # Sort by Retail Price
         return Response(response=json.dumps(response), status=200, mimetype="application/json")
     elif result:
-        result.sort(key=lambda x:x["Retail Bottle Price"])  # Sort by Retail Price
+        result.sort(key=lambda x: x["Retail Bottle Price"])  # Sort by Retail Price
         return Response(response=json.dumps(result), mimetype="application/json")
     else:
         print("firebase query failed")
